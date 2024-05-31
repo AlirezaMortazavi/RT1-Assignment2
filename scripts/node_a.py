63ef6ff8fbeaea37568bcf16d098d4e69952c189
AlirezaMortazavi <69080319+AlirezaMortazavi@users.noreply.github.com>
2024-05-31 14:40:22 +0000

    Add files via upload

--- scripts/node_a.py
+++ scripts/node_a.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 
+"""
+.. module:: rt1_assignment2
+   :platform: Unix
+   :synopsis: Python module for the node_a
+.. author:: Seyed Alireza Mortazavi
+
+Subscriber:
+    - /node_a/pose: Subscribes to the pose.
+"""
+
 # Import Libraries
 import rospy
 from nav_msgs.msg import Odometry
 import actionlib
 from assignment_2_2023.msg import Vel, PlanningAction, PlanningGoal
 
-def ros_components_init():
+def ros_components_init():   
     """
-    Initialize ROS components - publisher and action client.
-    
+    Initialize ROS components including the publisher and action client.
+
+    This function sets up the necessary ROS components for the application:
+    - A publisher for publishing the robot's position and velocity.
+    - An action client for sending goals to the 'reaching_goal' action server.
+    - A flag to indicate whether the current goal has been cancelled.
+
     Returns:
         tuple: A tuple containing:
             - pub (rospy.Publisher): The ROS publisher for position and velocity.
@@ -26,6 +41,8 @@ def user_input():
     """
     Prompt the user for input to either set a new goal or cancel the current goal.
 
+    This function interacts with the user through the terminal to receive commands for setting or cancelling goals.
+
     Returns:
         str: The user's input command ('s' for setting a new goal, 'c' for cancelling the current goal).
     """
@@ -34,7 +51,10 @@ def user_input():
 def process_goal_commands(pub, client, goal_cancelled):
     """
     Handle goal commands based on user input, either setting new goals or cancelling existing ones.
-    
+
+    This function continuously listens for user commands to either set a new goal or cancel the current goal. It subscribes
+    to the /odom topic to receive the robot's current odometry information, which it uses to update the robot's position and velocity.
+
     Args:
         pub (rospy.Publisher): The ROS publisher for position and velocity.
         client (actionlib.SimpleActionClient): The ROS action client for the 'reaching_goal' action.
@@ -79,7 +99,9 @@ def process_goal_commands(pub, client, goal_cancelled):
 def publish_position_velocity(msg, pub):
     """
     Publish position and velocity based on the received odometry message.
-    
+
+    This function extracts the robot's current position and velocity from the odometry message and publishes it using the provided publisher.
+
     Args:
         msg (nav_msgs.msg.Odometry): The odometry message containing position and velocity data.
         pub (rospy.Publisher): The ROS publisher for position and velocity.
@@ -99,6 +121,9 @@ def publish_position_velocity(msg, pub):
 def main():
     """
     Initialize the ROS node and start handling goal commands.
+
+    This is the main entry point of the script. It initializes the ROS node, sets up the necessary components,
+    and starts the process of handling user commands for setting and cancelling goals.
     """
     rospy.init_node('set_target_client')
     pub, client, goal_cancelled = ros_components_init()
